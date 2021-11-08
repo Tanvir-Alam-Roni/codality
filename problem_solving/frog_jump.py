69eb0d4ddd1d74db6114878146b90f489da9c999
Tanvir <tanvir.roni1997@gmail.com>
2021-11-08 20:58:48 +0600

    2nd commit

--- problem_solving/frog_jump.py
+++ problem_solving/frog_jump.py
@@ -1,8 +1,13 @@
+# def solution(X, Y, D):
+#     jump = 0
+#     while (X<Y):
+#         X += D
+#         jump += 1
+#     return jump
+
 def solution(X, Y, D):
-    jump = 0
-    while (X<Y):
-        X += D
-        jump += 1
+    jump = ((Y-X) // D) + 1
     return jump
+
 
 print(solution(10, 85, 30))
