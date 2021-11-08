dea0f7ef836d1e9b646da0e95b53ebd7ed947ccf
Tanvir <tanvir.roni1997@gmail.com>
2021-11-09 00:06:12 +0600

    handling empty list

--- problem_solving/perm_missing_elem.py
+++ problem_solving/perm_missing_elem.py
@@ -1,10 +1,21 @@
+# def solution(A):
+#     # write your code in Python 3.6
+#     A.sort()
+#     for i in range(len(A)):
+#         if A[i] != i+1:
+#             return i+1
+
 def solution(A):
     # write your code in Python 3.6
-    A.sort()
-    for i in range(len(A)):
-        if A[i] != i+1:
-            return i+1
-
-A = [2, 3, 1, 5, 6, 7, 9, 4]
+    if len(A)>0:
+        A.sort()
+        i = 0
+        while(i<len(A)):
+            if A[i] != i+1:
+                return i+1
+            i += 1
+    return 1
+    
+A = []
 
 print(solution(A))
