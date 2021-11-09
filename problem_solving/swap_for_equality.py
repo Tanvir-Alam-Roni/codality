5e7d9a3bcfd8f86646fe99f03561adc08432a2ea
Tanvir <tanvir.roni1997@gmail.com>
2021-11-09 17:57:06 +0600

    n^3 complexity

--- problem_solving/swap_for_equality.py
+++ problem_solving/swap_for_equality.py
@@ -1,14 +1,17 @@
 def swap_solution(A, B):
     l = len(A)
     for i in range(l):
-        A_ch = A.copy()
-        B_ch = B.copy()
-        A_ch[i], B_ch[i] = B_ch[i], A_ch[i]
-        if sum(A_ch) == sum(B_ch):
-            return True
+        for j in range(l):
+            A_ch = A.copy()
+            B_ch = B.copy()
+            A_ch[i], B_ch[j] = B_ch[j], A_ch[i]
+            if sum(A_ch) == sum(B_ch):
+                print(i, j)
+                print(A_ch, B_ch)
+                return True
     return False
 
 
-a = [2, 2, 3,4]
-b = [2, 2, 3, 5]
+a = [4, 2, 2, 3, 1]
+b = [2, 2, 2, 3, 5]
 print(swap_solution(a, b))
