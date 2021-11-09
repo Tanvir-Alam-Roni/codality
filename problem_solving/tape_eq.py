4ff322a1cd943942d89210cd9490d97935335fc0
Tanvir <tanvir.roni1997@gmail.com>
2021-11-09 13:46:22 +0600

    time complexity log(n)

--- problem_solving/tape_eq.py
+++ problem_solving/tape_eq.py
@@ -1,10 +1,20 @@
+# def solution(A):
+#     dif = []
+#     if len(A)>1 :
+#         for p in range(1, len(A)):
+#             dif.append(abs(sum(A[:p]) - sum(A[p:])))
+#         return min(dif)
+#     return abs(A[0])
+
+
 def solution(A):
     dif = []
     if len(A)>1 :
-        for p in range(1, len(A)):
+        p = 1
+        while p < len(A):
             dif.append(abs(sum(A[:p]) - sum(A[p:])))
+            p += 1
         return min(dif)
     return abs(A[0])
-
 A = [3, 1, 2, 4, 3]
 print(solution(A))
