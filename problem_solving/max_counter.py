257bb3403f6d7cc27a5e12ffca6b3cee39bc834f
Tanvir <tanvir.roni1997@gmail.com>
2021-11-15 23:23:54 +0600

    88%

--- problem_solving/max_counter.py
+++ problem_solving/max_counter.py
@@ -3,9 +3,12 @@
 
 def solution(N, A):
     x = [0]*N
+    m = 0
     for el in A:
-        if el>N:
-            x = [max(x)]*N
+        if el == N+1:
+            x = [m]*N
         elif el >= 1 and el <= N:
             x[el-1] += 1
+            if x[el-1]>m:
+                m = x[el-1]
     return x
