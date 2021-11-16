43d010842126d4bc854e1b0ffd129e1abe6a99bf
Tanvir <tanvir.roni1997@gmail.com>
2021-11-16 12:09:12 +0600

    77%

--- problem_solving/missing_value.py
+++ problem_solving/missing_value.py
@@ -1,11 +1,18 @@
 def solution(A):
-    flag = 0
-    i = 1
-    while flag==0:
-        if i not in set(A):
-            return i
+    A.sort()
+    A = list(set(A))
+    i = 0
+    if A[0]>1:
+        return 1
+    while(i < len(A)-1):   
+        if (A[i]<0 and A[i+1]>1):
+            return 1
+        if(A[i]>=0 and A[i+1] != A[i]+1):
+            return A[i]+1         
         i += 1
+    if A[i]<0:     
+        return 1  
+    return A[i]+1
 
-A = list(range(100))
-
+A = [0, 2, 1000]        
 print(solution(A))
